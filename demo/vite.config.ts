9a626d4d200d2a40be97ef5c0729edb04b3121e7
tagbug <41150861+tagbug@users.noreply.github.com>
2024-03-18 14:19:07 +0800

    chore: fix demo build

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import dotenv from "dotenv";
+import RollupPluginDotenv from "rollup-plugin-dotenv";
+import RollupPluginPolyfillNode from "rollup-plugin-polyfill-node";
+import EnvironmentPlugin from "vite-plugin-environment";
+
+const result = dotenv.config();
+console.log({ env: result.parsed });
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    react(),
+    // provider .env to process.env (only AVAILABLE in DEV mode)
+    EnvironmentPlugin(Object.keys(result.parsed)),
+  ],
   resolve: {
     dedupe: ["react", "react-dom"],
     alias: {
@@ -11,7 +23,29 @@ export default defineConfig({
   },
   build: {
     target: "es2020",
-    sourcemap: true
+    // sourcemap: true,
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        RollupPluginPolyfillNode({
+          include: null,
+        }),
+        // provider .env to process.env (only AVAILABLE in PROD mode)
+        RollupPluginDotenv(),
+      ],
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+      ] as any[],
+    },
   },
   server: {
     port: 5173,
